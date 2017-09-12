d5eac58a8917d3a6d3d0f8e0d11f23dc202aafe3
dragon <v.chemodurov@yandex.ru>
2017-09-12 18:56:41 +0300

    +add Synch democlass

---
+++ src/Synch.java
@@ -0,0 +1,38 @@
+public class Synch {
+    private static class CallMe {
+        void call(String msg) {
+            System.out.println("{" + msg);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.err.println("Interrupted!!!");
+            }
+            System.out.println("}");
+        }
+    }
+
+    private static class Caller implements Runnable {
+        String msg;
+        CallMe target;
+        Thread thread;
+
+        Caller(CallMe target, String msg){
+            this.target = target;
+            this.msg = msg;
+            thread = new Thread(this);
+            thread.start();
+        }
+
+        @Override
+        public void run() {
+            target.call(msg);
+        }
+    }
+
+    public static void main(String[] args) {
+        CallMe target = new CallMe();
+        Caller ob1 = new Caller(target, "Welcome");
+        Caller ob2 = new Caller(target, "to synchronized");
+        Caller ob3 = new Caller(target, "World!");
+    }
+}
