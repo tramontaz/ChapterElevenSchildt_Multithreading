23271ba34c599b15eab27d55712d6dc3f8ea572a
dragon <v.chemodurov@yandex.ru>
2017-09-14 18:37:11 +0300

    I fixed error. Reason: I have created nested classes (Вложенные классы). I have fixed that.

--- src/SuspendResume.java
+++ src/SuspendResume.java
@@ -1,45 +1,4 @@
 public class SuspendResume {
-    static class NewThread implements Runnable {
-        String threadName;
-        Thread thread;
-        boolean suspendFlag;
-
-        NewThread(String tName) {
-            threadName = tName;
-            thread = new Thread(this, threadName);
-            System.out.println("The name of New thread is " + threadName);
-            suspendFlag = false;
-            thread.start();
-        }
-
-        @Override
-        public void run() {
-            try {
-                for (int i = 15; i > 0; i--) {
-                    System.out.println(threadName + ": " + i);
-                    Thread.sleep(200);
-                    synchronized (this) {
-                        while (suspendFlag) {
-                            wait();
-                        }
-                    }
-                }
-            } catch (InterruptedException e) {
-                System.err.println(threadName + " have interrupted.");
-            }
-            System.out.println(threadName + " have finished");
-        }
-
-        synchronized void mySuspend() {
-            suspendFlag = true;
-        }
-
-        synchronized void myResume() {
-            suspendFlag = false;
-            notify();
-        }
-    }
-
     public static void main(String[] args) {
         NewThread ob1 = new NewThread("One");
         NewThread ob2 = new NewThread("Two");
@@ -70,6 +29,47 @@ public class SuspendResume {
         }
 
         System.out.println("The Main thread have ended.");
+    }
+}
+
+class NewThread implements Runnable {
+    String threadName;
+    Thread thread;
+    boolean suspendFlag;
+
+    NewThread(String tName) {
+        threadName = tName;
+        thread = new Thread(this, threadName);
+        System.out.println("The name of New thread is " + threadName);
+        suspendFlag = false;
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 15; i > 0; i--) {
+                System.out.println(threadName + ": " + i);
+                Thread.sleep(200);
+                synchronized (this) {
+                    while (suspendFlag) {
+                        wait();
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            System.err.println(threadName + " have interrupted.");
+        }
+        System.out.println(threadName + " have finished");
+    }
+
+    synchronized void mySuspend() {
+        suspendFlag = true;
+    }
+
+    synchronized void myResume() {
+        suspendFlag = false;
+        notify();
     }
 }
 
