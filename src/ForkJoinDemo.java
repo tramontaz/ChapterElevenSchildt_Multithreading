68ec8fe61bceb88885d05487479f3dfbbf1dae1a
dragon <v.chemodurov@yandex.ru>
2017-09-16 01:14:42 +0300

    +add demo class ForkJoin.

--- src/ForkJoinDemo.java
+++ src/ForkJoinDemo.java
@@ -1,2 +1,56 @@
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
 public class ForkJoinDemo {
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        double[] nums = new double[100_000];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (double) i;
+        }
+        System.out.print("the part of the original sequence: ");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+        SqrtTransform task = new SqrtTransform(nums, 0, nums.length);
+
+        forkJoinPool.invoke(task);
+
+        System.out.print("the part of the original sequence: ");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.format("%.4f ", nums[i]);
+        }
+        System.out.println();
+    }
+
+}
+
+class SqrtTransform extends RecursiveAction {
+    private final int seqThreshold = 1000;
+
+    private double[] data;
+    private int start, end;
+
+    public SqrtTransform(double[] data, int start, int end) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected void compute() {
+        if ((end - start) < seqThreshold) {
+            for (int i = start; i < end; i++) {
+                data[i] = Math.sqrt(data[i]);
+            }
+        } else {
+            int middle = (start + end) / 2;
+            invokeAll(new SqrtTransform(data, start, middle), new SqrtTransform(data, middle, end));
+        }
+    }
 }
