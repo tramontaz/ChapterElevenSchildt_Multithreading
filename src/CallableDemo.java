714001f8dfdb6ac384f45678cf013d9bbe12f587
dragon <v.chemodurov@yandex.ru>
2017-09-15 17:24:05 +0300

    +add demo class Callable.

---
+++ src/CallableDemo.java
@@ -0,0 +1,72 @@
+import java.util.concurrent.*;
+
+public class CallableDemo {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Future<Integer> futureSum;
+        Future<Double> futureLengthOfTheHypotenuse;
+        Future<Integer> futureFactorial;
+
+        System.out.println("Launching");
+
+        futureSum = executorService.submit(new Sum(10));
+        futureLengthOfTheHypotenuse = executorService.submit(new Hypotenuse(3, 4));
+        futureFactorial = executorService.submit(new Factorial(5));
+
+        try {
+            System.out.println(futureSum.get());
+            System.out.println(futureLengthOfTheHypotenuse.get());
+            System.out.println(futureFactorial.get());
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+        System.out.println("Completion.");
+    }
+}
+
+class Sum implements Callable<Integer> {
+    private int stop;
+
+    Sum(int stop) {
+        this.stop = stop;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        for (int i = 0; i < stop; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
+
+class Hypotenuse implements Callable<Double> {
+    private double theFirstSide, theSecondSide;
+
+    Hypotenuse(double theFirstSide, double theSecondSide) {
+        this.theFirstSide = theFirstSide;
+        this.theSecondSide = theSecondSide;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        return Math.sqrt((theFirstSide * theFirstSide) + (theSecondSide*theSecondSide));
+    }
+}
+
+class Factorial implements Callable<Integer> {
+    private int stop;
+
+    Factorial(int stop) {this.stop = stop;}
+
+    @Override
+    public Integer call() throws Exception {
+        int factorial = 1;
+        for (int i = 2; i <= stop; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
